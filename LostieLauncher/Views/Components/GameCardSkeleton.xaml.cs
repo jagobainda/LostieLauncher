be17645f7a22c3263e5aecd00853db76cf7ff1e4
jagobainda <jagobainda@gmail.com>
2026-04-22 14:30:13 +0200

    Añadir carga-esqueleto en todas las secciones

---
+++ LostieLauncher/Views/Components/GameCardSkeleton.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace LostieLauncher.Views.Components;
+
+public partial class GameCardSkeleton : UserControl
+{
+    public GameCardSkeleton()
+    {
+        InitializeComponent();
+    }
+}
---
+++ LostieLauncher/Views/Components/NewsCardSkeleton.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace LostieLauncher.Views.Components;
+
+public partial class NewsCardSkeleton : UserControl
+{
+    public NewsCardSkeleton()
+    {
+        InitializeComponent();
+    }
+}
